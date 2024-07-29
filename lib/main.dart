e10ca7f4feb7d8164af10a6375bc8f0eb2d3b8b7
603-20\kccistc <jonny995483@gmail.com>
2024-07-29 17:33:51 +0900

    task

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stack test'),
+        ),
+        body: Center(
+          child: Stack(
+            children: [
+
+              Positioned(
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  height: 312,
+                  width: 144,
+                  color: Colors.grey,
+                ),
+              ),
+
+              Positioned(
+                left:0,
+                bottom: 0,
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  height: 50,
+                  width: 144,
+                  color: Colors.brown,
+                ),
+              ),
+
+              Positioned(
+                left:10,
+                bottom: 50,
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  height: 40,
+                  width: 40,
+                  color: Colors.yellow,
+                ),
+              ),
+
+              Positioned(
+                left: 35,
+                top: 100,
+
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  height: 30,
+                  width: 72,
+                  color: Colors.lightBlueAccent,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(),
     );
   }
 }
