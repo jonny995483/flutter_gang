9c691a3a021c17542b1a3ea0d6fa0e395b230f19
603-20\kccistc <jonny995483@gmail.com>
2024-07-29 16:01:41 +0900

    하나더 추가

--- lib/column_row.dart
+++ lib/column_row.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -13,32 +11,47 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("안녕하세요"),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          title: const Text("안녕하세요"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width/2,
-                    height: 200,
-                    color:  Colors.blue,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width/2,
-                    height: 200,
-                    color:  Colors.red,
-                  ),
-                ]
-              )
-            ]
-          )
-      )
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width/2,
+                        height: 200,
+                        color: Colors.blue,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width/2,
+                        height: 200,
+                        color: Colors.red,
+                      ),
+                    ]
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width/2,
+                        height: 200,
+                        color: Colors.orange,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width/2,
+                        height: 200,
+                        color: Colors.green,
+                      ),
+                    ]
+                )
+              ]
+          ),
+        )
     );
   }
 }
